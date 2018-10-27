56de68e4ef3d7b9d190c351bcccf2d6f296cb3e3
password-manager <pawankumar17264@gmail.com>
2018-10-27 12:11:19 +0530

    linked bloks with other blocks

---
+++ blockchain.js
@@ -0,0 +1,51 @@
+let Blocks = require('./block');
+let SHA256 = require('js-sha256');
+let express = require('express');
+
+class Blockchain{
+        
+    constructor(defaultblock){
+        this.blocks = [];
+        this.addblock(defaultblock);
+    }
+     
+    addblock(block){
+     block.hash = this.gethash(block);
+     this.blocks.push(block);
+    }
+
+    generatenxtblock(block){
+         
+        let block = new Blocks();
+        let prevblock = this.getprevblock();
+
+        block.index = this.blocks.length;
+
+        block.prevhash = prevblock.hash;
+        block.hash = this.gethash(block);
+
+        return block;
+    }
+    
+    gethash(block){
+
+        let blockhash = SHA256(block.key);
+        
+        while(!blockhash.startwith('000')){
+            
+            block.nonce += 1;
+            blockhash = SHA256(block.key);
+           
+
+        }
+
+        return blockhash;
+
+    }
+
+    getprevblock(block){
+        return this.blocks[this.blocks.length - 1];
+    }
+    
+}
+module.exports = Blockchain;
