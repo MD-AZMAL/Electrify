82634ccbc0bbd7d634fc0c9db72c8d73608ef5bd
password-manager <pawankumar17264@gmail.com>
2018-10-27 11:17:03 +0530

    Added block

---
+++ block.js
@@ -0,0 +1,26 @@
+class Blocks{
+    
+    constructor(index,prevhash,senderkey,receiverkey,power,money){
+
+        this.index=index;
+        this.prevhash=prevhash;
+        this.hash="";
+        this.nonce=0;
+        this.data={
+            senderkey:senderkey,
+            receiverkey:receiverkey,
+            power:power,
+            money:money
+      };
+    }
+
+    get key(){
+        return JSON.stringify(this.data) + this.index + this.prevhash + this.hash + this.nonce;
+    }
+
+    addata(data){
+      this.data.push(data);    
+    }
+             
+}
+module.exports=Blocks;
